9edf9b0b2b4b905f5aca38e6c300efcf19f08737
Tim van Osch <timvosch@pollexprojects.nl>
2018-10-19 22:10:42 +0200

    Have TransactionLeg parse entity properties correctly

--- src/entities/TransactionLeg.js
+++ src/entities/TransactionLeg.js
@@ -1,18 +1,31 @@
-export default class TransactionLeg {
+import RevolutEntity from './RevolutEntity';
+import Account from './Account';
+import CounterParty from './CounterParty';
+import CounterAccount from './CounterAccount';
 
-    constructor(data) {
-        this.update(data);
-    }
+export default class TransactionLeg extends RevolutEntity {
 
     update(data) {
+        if (!super.update(data)) return;
         this.amount = data.amount;
         this.currency = data.currency;
         this.description = data.description;
 
-        // Relations
-        this.account = data.account;
-        this.counterParty = data.counterparty;
-        this.counterAccount = data.counterparty.account_id;
+        this.account = new Account({
+            broker: this.broker,
+            id: data.account_id
+        });
+        this.counterParty = new CounterParty({
+            broker: this.broker,
+            id: data.counterparty.id
+        });
+
+        // FIXME:BUG: CounterAccounts are not retrievable independently
+        // needs a counterParty, but above counterParty is a pending promise
+        this.counterAccount = new CounterAccount({
+            broker: this.broker,
+            id: data.counterparty.account_id
+        });
     }
 
 }
