f475bc995d9d4c0dcf332121f27c67a783482ec9
Tim van Osch <timvosch@pollexprojects.nl>
2018-10-19 18:34:12 +0200

    Add update for CounterParty and CounterAccounts

--- src/CounterAccount.js
+++ src/CounterAccount.js
@@ -6,4 +6,19 @@ export default class CounterAccount extends RevolutEntity {
         throw new Error('You should retrieve CounterAccounts through CounterParty entity');
     }
 
+    update(data) {
+        super.update(data);
+        this.accountNo = data.account_no;
+        this.routingNumber = data.routingNumber;
+        this.sortCode = data.sort_code;
+        this.iban = data.iban;
+        this.bix = data.bic;
+        this.type = data.type;
+        this.email = data.email;
+        this.name = data.name;
+        this.bankCountry = data.bank_country;
+        this.currency = data.currency;
+        this.recipient_charges = data.recipient_charges;
+    }
+
 }
--- src/CounterParty.js
+++ src/CounterParty.js
@@ -1,9 +1,24 @@
 import RevolutEntity from './RevolutEntity';
+import CounterAccount from './CounterAccount';
 
 export default class CounterParty extends RevolutEntity {
 
-    static GetResourcePath() {
-        return '/counterparty';
+    static GetResourcePath(id) {
+        return id? `/counterparty/${id}`:'/counterparties';
+    }
+
+    update(data) {
+        super.update(data);
+        this.name = data.name;
+        this.state = data.state;
+
+        this.updateAccounts(data.accounts);
+    }
+
+    updateAccounts(accountsData) {
+        this.accounts = accountsData.map(
+            accountData => new CounterAccount(accountData, this.broker)
+        );
     }
 
 }
