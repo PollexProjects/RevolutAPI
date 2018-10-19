e7fbd0c49fc7644ddaede3d0b397d7246c27e2af
Tim van Osch <timvosch@pollexprojects.nl>
2018-10-19 22:30:25 +0200

    Update documentation

--- src/RevolutBroker.js
+++ src/RevolutBroker.js
@@ -3,6 +3,12 @@ const url = require('url');
 
 export default class RevolutBroker {
 
+    /**
+     * The RevolutBroker is responsible for final communication between the API
+     * and the facade
+     * @param {string}  key           Your Revolut Business API Key
+     * @param {Boolean} [debug=false] Whether you're using Revolut's sandbox API
+     */
     constructor(key, debug = false) {
         this.apiKey = key;
         this.debug = debug;
@@ -16,10 +22,15 @@ export default class RevolutBroker {
         })
     }
 
+    /**
+     * Fetches all or a single RevolutEntity from the API
+     * @param  {RevolutEntity}  resource    The entity to fetch
+     * @param  {UUID}  id                   Optional ID will fetch a specific entity
+     * @return {Promise}                    Promise resolving in the given resource
+     */
     async getResource({ resource, id }) {
         try {
             const { data } = await this.axios.get(resource.GetResourcePath(id))
-
             // Map data
             if (Array.isArray(data)) {
                 return data.map(
@@ -31,7 +42,6 @@ export default class RevolutBroker {
             } else {
                 return new resource({ broker: this, data });
             }
-
         } catch(error) {
             console.error(error);
         }
