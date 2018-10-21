6dc4f5e3a653b3e9e74b969f0d1ecd0ad933fe68
Tim van Osch <timvosch@pollexprojects.nl>
2018-10-21 12:37:22 +0200

    Add ability to transfer directly from Account entity

--- src/entities/Account.js
+++ src/entities/Account.js
@@ -1,4 +1,5 @@
 import RevolutEntity from './RevolutEntity';
+import Transfer from '../Transfer';
 
 export default class Account extends RevolutEntity {
 
@@ -14,5 +15,24 @@ export default class Account extends RevolutEntity {
         this.currency = data.currency;
         this.state = data.state;
         this.public = data.public;
+    }
+
+    /**
+     * Create a Transfer from one account to another. This transfer is not
+     * yet executed.
+     * @param  {Account} to        The receiving account
+     * @param  {decimal} amount    The amount to send
+     * @param  {string} reference  An optional reference for the transfer
+     * @return {Transfer}          A Transfer entity
+     */
+    transfer(to, amount, reference = '') {
+        return new Transfer({
+            broker: this.broker,
+            source: this,
+            target: to,
+            amount,
+            reference,
+            currency: this.currency
+        });
     }
 }
